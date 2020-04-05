0651aaed315a700b83593e2a200e7d989e50564c
Hrant Poghosyan <hrant@live.ru>
2020-04-05 17:33:07 +0400

    build production

--- webpack.config.js
+++ webpack.config.js
@@ -1,136 +1,133 @@
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
-    .BundleAnalyzerPlugin;
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = (env = {}) => {
-    const { mode = "development" } = env;
-    const isProd = mode === "production";
-    const isDev = mode === "development";
+  const { mode = 'development' } = env;
+  const isProd = mode === 'production';
+  const isDev = mode === 'development';
 
-    const getStyleLoaders = () => {
-        return [
-            isProd ? MiniCssExtractPlugin.loader : "style-loader",
-            "css-loader",
-            "sass-loader"
-        ];
-    };
+  const getStyleLoaders = () => [
+    isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+    'css-loader',
+    'sass-loader'
+  ];
 
-    const getPlugins = () => {
-        const plugins = [
-            new HtmlWebpackPlugin({
-                title: "App",
-                template: "public/index.html",
-                hash: true
-            })
-        ];
+  const getPlugins = () => {
+    const plugins = [
+      new HtmlWebpackPlugin({
+        title: 'App',
+        template: 'public/index.html',
+        hash: true
+      })
+    ];
 
-        if (isProd) {
-            plugins.push(
-                new BundleAnalyzerPlugin(),
-                new CleanWebpackPlugin(),
-                new MiniExtractPlugin({
-                    filename: "css/main-[hash:8].css"
-                })
-            );
-        }
-
-        if (isDev) {
-            plugins.push(new webpack.SourceMapDevToolPlugin({}));
-        }
-
-        return plugins;
-    };
-
-    const config = {
-        mode: isProd ? "production" : isDev && "development",
-
-        output: {
-            filename: isProd ? "js/main-[hash:8].js" : undefined
-        },
-
-        module: {
-            rules: [
-                {
-                    test: /\.js?$/,
-                    exclude: /node_modules/,
-                    use: [
-                        {
-                            loader: "babel-loader"
-                        }
-                    ]
-                },
-
-                // Loading CSS
-                {
-                    test: /\.css$/i,
-                    use: ['style-loader', ...getStyleLoaders()]
-                },
-
-                // Loading SASS/SCSS
-                {
-                    test: /\.s[ca]ss$/i,
-                    use: getStyleLoaders()
-                },
-
-                // Loading images
-                {
-                    test: /\.(svg|png|jpe?g|gif|ico)$/i,
-                    use: [
-                        {
-                            loader: "url-loader",
-                            options: {
-                                outputPath: "images",
-                                limit: 8192,
-                                name: "[name]-[sha1:hash:7].[ext]"
-                            }
-                        }
-                    ]
-                },
-
-                // Loading sounds
-                {
-                    test: /\.(mp3)$/,
-                    loader: "file-loader"
-                },
-
-                // Loading fonts
-                {
-                    test: /\.(ttf|otf|eot|woff|woff2)$/i,
-                    use: [
-                        {
-                            loader: "file-loader",
-                            options: {
-                                outputPath: "fonts",
-                                name: "[name].[ext]"
-                            }
-                        }
-                    ]
-                }
-            ]
-        },
-        plugins: getPlugins(),
-        devServer: {
-            hot: true,
-            historyApiFallback: true,
-            progress: true,
-            open: "chrome"
-        }
-    };
-
-    // Optimization
     if (isProd) {
-        config.optimization = {
-            minimizer: [
-                new UglifyJsPlugin({
-                    sourceMap: true
-                })
-            ]
-        };
+      plugins.push(
+        new BundleAnalyzerPlugin(),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+          filename: 'css/main-[hash:8].css'
+        })
+      );
     }
 
-    return config;
+    if (isDev) {
+      plugins.push(new webpack.SourceMapDevToolPlugin({}));
+    }
+
+    return plugins;
+  };
+
+  const config = {
+    mode: isProd ? 'production' : isDev && 'development',
+
+    output: {
+      filename: isProd ? 'js/main-[hash:8].js' : undefined
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.js?$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader'
+            }
+          ]
+        },
+
+        // Loading CSS
+        {
+          test: /\.css$/i,
+          use: ['style-loader', ...getStyleLoaders()]
+        },
+
+        // Loading SASS/SCSS
+        {
+          test: /\.s[ca]ss$/i,
+          use: getStyleLoaders()
+        },
+
+        // Loading images
+        {
+          test: /\.(svg|png|jpe?g|gif|ico)$/i,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                outputPath: 'images',
+                limit: 8192,
+                name: '[name]-[sha1:hash:7].[ext]'
+              }
+            }
+          ]
+        },
+
+        // Loading sounds
+        {
+          test: /\.(mp3)$/,
+          loader: 'file-loader'
+        },
+
+        // Loading fonts
+        {
+          test: /\.(ttf|otf|eot|woff|woff2)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: 'fonts',
+                name: '[name].[ext]'
+              }
+            }
+          ]
+        }
+      ]
+    },
+    plugins: getPlugins(),
+    devServer: {
+      hot: true,
+      historyApiFallback: true,
+      progress: true,
+      open: 'chrome'
+    }
+  };
+
+  // Optimization
+  if (isProd) {
+    config.optimization = {
+      minimizer: [
+        new UglifyJsPlugin({
+          sourceMap: true
+        })
+      ]
+    };
+  }
+
+  return config;
 };
